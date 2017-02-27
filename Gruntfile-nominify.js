1890db74ad48397b8fdf8d575d80475d6ae1276f
blaazej3k <bialas.blazej@gmail.com>
2017-02-28 00:22:25 +0100

    28.02 54.154.154.100

--- Gruntfile-nominify.js
+++ Gruntfile-nominify.js
@@ -70,7 +70,7 @@ module.exports = function (grunt) {
       options: {
         port: 80,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: '54.154.154.100',
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
