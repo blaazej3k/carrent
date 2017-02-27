d856419a79f5192bfb30036acde37fd986d22f38
blaazej3k <bialas.blazej@gmail.com>
2017-02-28 00:26:28 +0100

    28.02 localhost

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,7 +70,7 @@ module.exports = function (grunt) {
       options: {
         port: 80,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: '0.0.0.0',
+        hostname: 'locahost',
         livereload: 35729
       },
       livereload: {
