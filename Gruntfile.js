d7f2f29a6d8e7ae64fc1ef8c549d7629f7700beb
blaazej3k <bialas.blazej@gmail.com>
2017-02-28 11:43:07 +0100

    28.02 grunt serve

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,9 +68,9 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 80,
+        port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'locahost',
+        hostname: 'localhost',
         livereload: 35729
       },
       livereload: {
